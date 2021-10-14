2949989545275ac47d94c55a9ef858bc34c36240
alvaro lopes <alvim.lopez@ gmail.com>
2021-10-14 10:51:29 -0300

    iniciando o projeto

---
+++ src/main/java/com/alvim/GerenciamentProduto/GerenciamentProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.alvim.GerenciamentProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentProdutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvim/GerenciamentProduto/GerenciamentProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvim.GerenciamentProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
