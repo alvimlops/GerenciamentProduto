2ca46b80d0a37939dd51ffdd712b81e76a812599
alvaro lopes <alvim.lopez@ gmail.com>
2021-10-15 10:56:32 -0300

    Implementando a class de repositories, e testando local

--- src/main/java/com/alvim/GerenciamentProduto/resource/CategoryResource.java
+++ src/main/java/com/alvim/GerenciamentProduto/resource/CategoryResource.java
@@ -3,6 +3,7 @@ package com.alvim.GerenciamentProduto.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,23 +11,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alvim.GerenciamentProduto.entities.Category;
+import com.alvim.GerenciamentProduto.repositories.CategoryRepository;
 
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+	@Autowired
+	private CategoryRepository categoyRepository;
+	
+	
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List <Category> list = new ArrayList<>();
-		list.add(new Category(1L, "eletronics"));
-		list.add(new Category(1L, "books"));
+		List <Category> list = categoyRepository.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
-/*	@GetMapping(value = "/{id}")
-	public ResponseEntity<...> findById(@PathVariable Long id) {
-		...
-		return ResponseEntity.ok().body(...);
-	}*/
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category cat = categoyRepository.findById(id);
+		return ResponseEntity.ok().body(cat);
+	}
 }
